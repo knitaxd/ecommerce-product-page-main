0752283043a6189814a2f91ff308e5942bbdd322
Knita <ismoialex@gmail.com>
2022-04-06 10:35:06 -0400

    menu finished

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 const nextArrow = document.getElementById('next__icon')
+const touchSlide = document.querySelector('.main__slide--box')
 
 
-nextArrow.addEventListener('click', ()=>{
-    document.querySelector('.first').style.transition = '1.5s'
-    document.querySelector('.first').style.marginLeft = '-20%'
+// arrow functions
+let indexValue = 1
+showImg(indexValue)
+
+function sideSlide(e){
+    showImg(indexValue +=e)
+}
+
+function showImg(e){
+    const img = document.querySelectorAll('.product__img')
+    if(e > img.length) {
+        indexValue = 1
+    } else if(e < 1){
+        indexValue = img.length
+    }
+
+    for (let i = 0; i < img.length; i++) {
+        img[i].style.display = 'none';
+    }
+
+    img[indexValue - 1].style.display = 'block';
+}
+
+// nav functions
+document.querySelector(".nav__background").addEventListener('click', ()=>{
+    closeNav()
 })
+
+let openNav = () => {
+    document.getElementById("nav__deploy").style.width = "67%"
+    document.querySelector(".nav__background").style.width = "33%"
+ }
+let closeNav = () => {
+    document.getElementById("nav__deploy").style.width = "0"
+    document.querySelector(".nav__background").style.width = "0"
+}
+
